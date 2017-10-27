dc35ac51b5e8922de4d4f9f11cffa3b4c8deb8b7
Stéphane Campinas <stephane.campinas@gmail.com>
2017-10-27 10:32:59 +0100

    set drill's default value

--- java/memory/src/main/java/org/apache/arrow/memory/BoundsChecking.java
+++ java/memory/src/main/java/org/apache/arrow/memory/BoundsChecking.java
@@ -27,7 +27,7 @@ public class BoundsChecking {
     boolean isAssertEnabled = false;
     assert isAssertEnabled = true;
     BOUNDS_CHECKING_ENABLED = isAssertEnabled
-        || !"true".equals(System.getProperty("drill.enable_unsafe_memory_access"));
+        || !"true".equals(System.getProperty("siren.drill.enable_unsafe_memory_access", "true"));
   }
 
   private BoundsChecking() {
