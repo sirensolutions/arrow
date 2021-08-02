fe8a7a677dee95659a552a5c509b468012ce0f1c
George Apaaboah <george.apaaboah@gmail.com>
2021-08-02 19:46:02 +0200

    Revert changes

--- java/vector/src/main/java/org/apache/arrow/vector/ipc/JsonFileWriter.java
+++ java/vector/src/main/java/org/apache/arrow/vector/ipc/JsonFileWriter.java
@@ -378,7 +378,7 @@ public class JsonFileWriter implements AutoCloseable {
         }
         case DECIMAL: {
           int scale = ((DecimalVector) vector).getScale();
-          BigDecimal decimalValue = DecimalUtility.getBigDecimalFromArrowBuf(buffer, index, scale,
+          BigDecimal decimalValue = DecimalUtility.getBigDecimalFromArrowBuf(buffer, index, scale, 
                                                                              DecimalVector.TYPE_WIDTH);
           // We write the unscaled value, because the scale is stored in the type metadata.
           generator.writeString(decimalValue.unscaledValue().toString());
@@ -386,7 +386,7 @@ public class JsonFileWriter implements AutoCloseable {
         }
         case DECIMAL256: {
           int scale = ((Decimal256Vector) vector).getScale();
-          BigDecimal decimalValue = DecimalUtility.getBigDecimalFromArrowBuf(buffer, index, scale,
+          BigDecimal decimalValue = DecimalUtility.getBigDecimalFromArrowBuf(buffer, index, scale, 
                                                                              Decimal256Vector.TYPE_WIDTH);
           // We write the unscaled value, because the scale is stored in the type metadata.
           generator.writeString(decimalValue.unscaledValue().toString());
